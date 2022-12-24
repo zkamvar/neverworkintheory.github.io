483a22743c878e66e58f63721004e06e72f5d35f
Greg Wilson <gvwilson@third-bit.com>
2022-12-24 09:47:58 -0500

    Simplifying site

--- bin/authors.py
+++ bin/authors.py
@@ -2,43 +2,75 @@
 
 """Construct table of articles by author."""
 
+import argparse
+from pybtex.database import parse_string as pybtex_parse_string
 import sys
-from pybtex.database import parse_string
+import yaml
 
-from util import LATEX_CHARS
+
+LATEX_CHARS = (
+    ('---', '—'),
+    (r"\`{A}", 'À'),
+    (r"\`{E}", 'È'),
+    (r"\'{E}", 'É'),
+    (r"\'{S}", 'Ś'),
+    (r"\'{a}", 'á'),
+    (r"\'{c}", 'ć'),
+    (r"\`{a}", 'à'),
+    (r"\`{e}", 'è'),
+    (r"\'{e}", 'é'),
+    (r"\'{i}", 'í'),
+    (r"\'{o}", 'ó'),
+    (r"\'{u}", 'ú'),
+    (r"\'{\i}", 'í'),
+    (r'\"{a}', 'ä'),
+    (r'\"{o}', 'ö'),
+    (r'\"{u}', 'ü'),
+    (r'\'{O}', 'Ó'),
+    (r'\'{o}', 'ó'),
+    (r'\c{C}', 'Ç'),
+    (r'\c{c}', 'ç'),
+    (r'\v{S}', 'Š'),
+    (r'\v{e}', 'ě'),
+    (r'\v{r}', 'ř'),
+    (r'\v{s}', 'š'),
+    (r'\v{z}', 'ž'),
+    (r'\~{a}', 'ã'),
+    (r'\~{n}', 'ñ'),
+    (r'{\AA}', 'Å'),
+    (r'{\aa}', 'å'),
+    (r'{\o}', 'ø'),
+    (r'\%', '%'),
+    (r'\#', '#'),
+    (r'${\approx}$', '≈'),
+    (r'${\pm}$', '±'),
+    (r'${\times}$', '×'),
+    (r'\textquotesingle', "'"),
+    (r'\textquotedblleft', "'"),
+    (r'\textquotedblright', "'"),
+    (r'{\ldots}', '…'),
+    (r'{\textemdash}', '—'),
+    (r'{\textendash}', '–'),
+    ('{', ''),
+    ('}', ''),
+    ('\$', '$'),
+    ('\\', ''),
+    ('  ', ' '),
+    ('&', '&amp;'),
+    ('<', '&lt;'),
+    ('>', '&gt;')
+)
+
 
 def main():
-    text = sys.stdin.read()
-    bib = parse_string(text, "bibtex")
-    bib = bib.entries
+    config = _parse_args()
+    bib = _read_bib(config)
+    unreviewed = _read_unreviewed(config)
+    bib = _remove_unreviewed(bib, unreviewed)
 
-    unreviewed = {x.strip() for x in open(sys.argv[1], "r").readlines()}
-
-    seen = {}
-    for key in sorted(bib.keys()):
-        if key in unreviewed:
-            continue
-
-        if "reviewed" not in bib[key].fields:
-            print(f"entry {key} missing 'reviewed'", file=sys.stderr)
-            sys.exit(1)
-
-        reviewed = bib[key].fields["reviewed"]
-        for p in bib[key].persons:
-            for name in bib[key].persons[p]:
-                seen.setdefault(_clean(name), set()).add((key, reviewed))
-
-    print("<table>")
-    print('<tr><th>Name</th><th>Citation</th><th>Reviewed</th></tr>')
-    for name in sorted(seen.keys(), key=lambda x: _sort(x)):
-        first = name
-        for (key, reviewed) in sorted(seen[name]):
-            _, year, month, day, _ = reviewed.split('/')
-            key = f'<a href="/bib/#{key}">{key}</a>'
-            reviewed = f'<a href="{{{{\'{reviewed}\' | relative_url}}}}">{year}-{month}-{day}</a>'
-            print(f"<tr><td>{first}</td><td>{key}</td><td>{reviewed}</td></tr>")
-            first = ""
-    print("</table>")
+    authors = _by_author(bib)
+    with open(config.authors, "w") as writer:
+        yaml.dump(authors, writer)
 
 
 def _clean(name):
@@ -48,8 +80,47 @@ def _clean(name):
     return name
 
 
-def _sort(name):
-    return name.replace("Å", "A").replace("É", "E").replace("Ś", "S").lower()
+def _by_author(bib):
+    """Convert entries to YAML of authors and entries."""
+    authors = {}
+
+    for key in sorted(bib.keys()):
+        reviewed = bib[key].fields["reviewed"]
+        for p in bib[key].persons:
+            for name in bib[key].persons[p]:
+                authors.setdefault(_clean(name), set()).add((key, reviewed))
+
+    return {a: [{r[0]:r[1]} for r in sorted(authors[a])] for a in authors}
+
+
+def _parse_args():
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--authors", help="Output authors YAML file")
+    parser.add_argument("--bib", help="Bibliography BibTeX file")
+    parser.add_argument("--strings", help="String definitions BibTeX file")
+    parser.add_argument("--unreviewed", help="Plaintext file with keys of unreviewed items")
+    return parser.parse_args()
+
+
+def _read_bib(config):
+    """Read bibliography and strings and convert."""
+    bib = open(config.bib, "r").read()
+    strings = open(config.strings, "r").read()
+    bib = strings + "\n\n" + bib
+    return pybtex_parse_string(bib, "bibtex").entries
+
+
+def _read_unreviewed(config):
+    """Read keys of unreviewed bibliography entries."""
+    lines = open(config.unreviewed, "r").readlines()
+    lines = [ln.strip() for ln in lines]
+    return {ln for ln in lines if ln}
+
+
+def _remove_unreviewed(bib, unreviewed):
+    """Remove unreviewed items from bibliography."""
+    return {k:bib[k] for k in bib.keys() if k not in unreviewed}
 
 
 if __name__ == "__main__":
